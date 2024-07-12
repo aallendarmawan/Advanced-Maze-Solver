c6eae0ce072fd8eb173e41f518a44c5a8d05055f
AllenD <110092516+aallendarmawan@users.noreply.github.com>
2024-07-12 11:39:38 +1000

    Update port configuration for Heroku

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    from waitress import serve
-    import logging
-    logger = logging.getLogger('waitress')
-    logger.setLevel(logging.DEBUG)
-    serve(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
